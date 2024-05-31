7c526baa8c194851b38630557865f864eae3c255
FarisMsr <mansourifaris52@gmail.com>
2024-05-31 09:55:18 +0200

    Changement total

--- Include/EnteteDePage.inc.php
+++ Include/EnteteDePage.inc.php
@@ -2,10 +2,38 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="./Style/style.css" />
-    <title> Acceuil</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ma Page PHP</title>
+    <link rel="stylesheet" href="Style\style.css">
 </head>
-    <body>
-        <H1>BIP BIP BAT</H1>
-    </body>" ;
-
+<body>
+    <header>
+    <div style="width: 100%; height: 100%; background: white; border-radius: 106.18px; border: 11.68px black solid"></div><div style="width: 100%; height: 100%; background: white; border-radius: 106.18px; border: 11.68px black solid"></div>
+        <div class="logo">
+            <img src="Images\Entete\LogoBipbipBat.webp" alt="Logo">
+        </div>
+        <div class="search-bar">
+            <input type="text" placeholder="Recherchez votre produit">
+            <button type="submit"><img src="Images\logo\logorecherche.png" alt="Rechercher"></button>
+        </div>
+        <div class="nav-icons">
+            <a href="#"><img src="Images\Entete\Comptelogo.png" alt="User"></a>
+            <a href="#"><img src="Images\Entete\Logo Like.png" alt="Wishlist"></a>
+            <a href="#"><img src="Images\Entete\panier.png" alt="Cart"></a>
+        </div>
+        <nav class="menu">
+            <ul>
+                <li><a href="#">ACCUEIL</a></li>
+                <li><a href="#">OUTILLAGES</a></li>
+                <li><a href="#">CONSOMMABLES</a></li>
+                <li><a href="#">PEINTURE</a></li>
+                <li><a href="#">CARRELAGE</a></li>
+                <li><a href="#">PLOMBERIE</a></li>
+                <li><a href="#">ÉLECTRICITÉ</a></li>
+                <li><a href="#">MENUISERIE</a></li>
+                <li><a href="#">PLAQUISTE</a></li>
+            </ul>
+        </nav>
+    </header>
+</body>
+</html>
