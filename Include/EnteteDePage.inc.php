4238148d192dc548565de8f12ee9e4cb5427632d
FarisMsr <mansourifaris52@gmail.com>
2024-05-31 09:55:33 +0200

    entete finale

--- Include/EnteteDePage.inc.php
+++ Include/EnteteDePage.inc.php
@@ -8,7 +8,6 @@
 </head>
 <body>
     <header>
-    <div style="width: 100%; height: 100%; background: white; border-radius: 106.18px; border: 11.68px black solid"></div><div style="width: 100%; height: 100%; background: white; border-radius: 106.18px; border: 11.68px black solid"></div>
         <div class="logo">
             <img src="Images\Entete\LogoBipbipBat.webp" alt="Logo">
         </div>
