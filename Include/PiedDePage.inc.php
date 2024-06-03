06355634615db25f8b66a1aea8bf67b1aab07e1e
FarisMsr <mansourifaris52@gmail.com>
2024-06-03 16:06:22 +0200

    footer

--- Include/PiedDePage.inc.php
+++ Include/PiedDePage.inc.php
@@ -1,2 +1,34 @@
-</body>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PiedDePage</title>
+    <link rel="stylesheet" href="Style/style.css">
+</head>
+    <footer>
+        <div id="conteneur">
+            <div class="info">
+                <h3>Contactez nous</h3>
+                <p>Mail 📧</p>
+                <p>Téléphone 📞</p>
+                <p>Devenir Livreur</p>
+            </div>
+            <div class="info">
+                <h3>Informations</h3>
+                <p>Politique de confidentialité</p>
+                <p>Vos données</p>
+            </div>
+            <div class="info">
+                <h3>Merci pour votre visite !</h3>
+                <p>@BIPBIPBAT 2024</p>
+            </div>  
+            <div class="info">
+                <h3>Suivi de Commande</h3>
+                <p>Comment ça marche ?</p>
+                <p>Politique RGPD</p>
+                <p>Laissez nous un avis</p>
+            </div>
+        </div>
+    </footer>
 </html>
