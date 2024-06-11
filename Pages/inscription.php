74849e3b6d421683ac988e25e6539c5e8882f4ca
FarisMsr <mansourifaris52@gmail.com>
2024-06-11 21:17:34 +0200

    v2

--- Pages/inscription.php
+++ Pages/inscription.php
@@ -58,19 +58,7 @@ if (isset($_POST["submit"])) {
 
         <br><br>
         <input type="submit" name="submit" value="Créer un compte">
-        <main class="main">
-        <div class="bannière">
-        <p id="bannière-text">Inscription </p> 
-        <img src="Images/background/Fond bois.jpg" alt="bois">
-        </div>
-        <div class="Chemin">
-        <P id="chemin-text"> <a href="Acceuil .php">ACCUEIL </a> > Inscription <p>
-        </div>
-        <label for="Trier-select">Trier par ordre:</label>
-        <select name="Trier" id="Trier-select">
-        <option value="">Trier par défaut</option>
-        <option value="Trier croissant">Trier par ordre croissant</option>
-        <option value="Trier décroissant">Trier par ordre décroissant</option>
+    
     </select>
     </main>
     </form>
